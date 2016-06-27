c8b5e15d5d28b66d44b0baf8a45ffa7999149d43
luiscalado <elcalado2010@hotmail.com>
2016-06-27 22:44:32 +0100

    initial samples

---
+++ demo.ps1
@@ -0,0 +1,16 @@
+﻿Get-AzureSubscription
+
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId "ff0d44a7-e7f6-4a10-bfdf-3cbba8eddc36"
+
+# Sample 1- Hello World
+New-AzureRmResourceGroup -Name "elcalado-hello" -Location "West Europe" -Force -Verbose
+New-AzureRmResourceGroupDeployment -ResourceGroupName "elcalado-hello" -TemplateFile "C:\code\armwebcast\helloWorld.json" -Verbose
+
+# Sample 2 - Storage Account
+New-AzureRmResourceGroup -Name "elcalado-store1" -Location "West Europe" -Force -Verbose
+New-AzureRmResourceGroupDeployment -ResourceGroupName "elcalado-store1" -TemplateFile "C:\code\armwebcast\storageaccount.json" -Verbose
+
+# Sample 3 - Web + SQL Dev / Test
+New-AzureRmResourceGroup -Name "elcalado-dev" -Location "West Europe" -Force -Verbose
+New-AzureRmResourceGroupDeployment -ResourceGroupName "elcalado-dev" -TemplateUri "C:\code\armwebcast\dev.json" -Verbose 
